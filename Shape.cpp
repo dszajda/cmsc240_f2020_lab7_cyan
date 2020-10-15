34040c3cf66e9f613cd3204605c865cf2c438b08
yingzhu-richmond <ying.zhu@richmond.edu>
2020-10-15 17:02:58 -0400

    initial start on creating shape.cpp and makefile

---
+++ Shape.cpp
@@ -0,0 +1,18 @@
+#ifndef __SHAPTE_CPP__
+#define __SHAPTE_CPP__
+
+#include "Shape.h"
+
+Shape::Shape(std::string shapeName){}
+Shape::~Shape(){}
+
+
+
+double Shape::getArea(){
+
+}
+
+
+void Shape::print(){
+  
+}
